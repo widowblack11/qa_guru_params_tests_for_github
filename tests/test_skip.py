8b25e908f0c2732df9a399eebc199c520f55d820
widowblack11 <oksana.prokopenko16@gmail.com>
2023-02-13 21:58:39 +0300

    change tests

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -7,10 +7,12 @@ from selene import be
 def test_for_signin_only_desktop(width, height):
     if height == 768 or width == 900:
         pytest.skip(reason='Размер бразуера не подходит для мобильной версии')
+
     browser.config.window_width = width
     browser.config.window_height = height
     browser.open('https://github.com/')
     browser.element('[href="/login"]').click()
+
     assert browser.element('#login').should(be.visible)
 
 
@@ -18,10 +20,12 @@ def test_for_signin_only_desktop(width, height):
 def test_for_signin_only_mobile(width, height):
     if width == 1024 or height == 1080:
         pytest.skip(reason='Размер бразуера не подходит для десктопной версии')
+
     browser.config.window_width = width
     browser.config.window_height = height
     browser.open('https://github.com/')
     browser.element('.Button-label').click()
     browser.element('[href="/login"]').click()
+
     assert browser.element('#login').should(be.visible)
 
