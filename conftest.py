f2e4de77299426d74071c6397c56ce22d182f2ef
widowblack11 <oksana.prokopenko16@gmail.com>
2023-02-13 21:58:53 +0300

    add fixture

--- conftest.py
+++ conftest.py
@@ -16,3 +16,15 @@ def open_browser_for_mobile():
     browser.config.window_height = 150
     yield
     browser.quit()
+
+
+@pytest.fixture(params=["desktop", "mobile"])
+def given_size(request):
+    if request.param == "desktop":
+        browser.config.window_height = 1920
+        browser.config.window_width = 1080
+    elif request.param == "mobile":
+        browser.config.window_height = 400
+        browser.config.window_width = 200
+
+    return request.param
