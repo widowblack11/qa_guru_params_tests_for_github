1db10af1506adecd840b8025bd5d94aa2dec8a31
widowblack11 <oksana.prokopenko16@gmail.com>
2023-02-13 21:58:16 +0300

    add tests with indirect

---
+++ tests/test_parametrize.py
@@ -0,0 +1,21 @@
+import pytest
+from selene import be
+from selene.support.shared import browser
+
+
+@pytest.mark.parametrize("given_size", ['desktop'], indirect=True)
+def test_login_desktop_indirect(given_size):
+    browser.open('https://github.com/')
+    browser.element('[href="/login"]').click()
+    
+    assert browser.element('#login').should(be.visible)
+
+
+@pytest.mark.parametrize("given_size", ["mobile"], indirect=True)
+def test_login_mobile_indirect(given_size):
+    browser.open('https://github.com/')
+    browser.element('.Button-label').click()
+    browser.element('[href="/login"]').click()
+
+    assert browser.element('#login').should(be.visible)
+
